cfe9f3601b8505d34428bc2371bc3a2964073891
Jake Radcliffe <jradcliffe@veracode.com>
2024-05-08 01:27:08 -0500

    add code

---
+++ src/main/java/com/example/cleanrepo/CleanRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cleanrepo/CleanRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
